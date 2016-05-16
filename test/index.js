2eec2cef418560d41308cec88822c2eec5145e7a
Donald Page <don.page.work@gmail.com>
2016-05-15 21:20:21 -0400

    Done basic color change and chaining prop.

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 import assert from 'assert';
 import kqJs from '../lib';
 
-describe('kq-js', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
-  });
-});
+// describe('kq-js', function () {
+//   it('should have unit test!', function () {
+//     assert(false, 'we expected this package author to add actual unit tests.');
+//   });
+// });
